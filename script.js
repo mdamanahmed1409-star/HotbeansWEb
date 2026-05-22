5e25874eebdbdbb002adc6184a937fdec06da771
MdAman <mdaman.ahmed1409@gmail.com>
2026-05-22 11:28:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let course = "HTML";
+console.log(course);
+
+let number = 0;
+
+if (number > 0) {
+    console.log(`${number} is greather than 0`)
+}
+
+else if (number < 0) {
+    console.log(`${number} is less than 0`)
+}
+
+else {
+    console.log(`${number} is less than 0`);  
+}
+
+let subject = "networking";
+console.log(subject)
